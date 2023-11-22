4e399bc460065958499d3c4ceee6ffd379bafa95
Mochammad Gamal <mogams1811@gmail.com>
2023-11-22 10:03:59 +0700

    refactor: :construction: implementasi basic auth
    penyesuaian algoritma pada halaman login serta penambahan route login pada form login, lalu pada navbar ada penyesuaian pemanggilan route logout dengan method get.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 @extends('templates.default')
 @php
-    $title = 'Login';
-    $preTitle = 'Login';
-    $currentRouteName = Route::currentRouteName();
+$title = 'Login';
+$preTitle = 'Login';
+$currentRouteName = Route::currentRouteName();
 @endphp
 
 @section('content')
@@ -25,7 +25,8 @@
                 <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="dist/assets/media/auth/agency-dark.png" alt="" />
                 <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">Fast, Efficient and Productive</h1>
                 <div class="text-gray-600 fs-base text-center fw-semibold">In this kind of post,
-                    <a href="#" class="opacity-75-hover text-primary me-1">the blogger</a>introduces a person they’ve interviewed
+                    <a href="#" class="opacity-75-hover text-primary me-1">the blogger</a>introduces a person they’ve
+                    interviewed
                     <br />and provides some background information about
                     <a href="#" class="opacity-75-hover text-primary me-1">the interviewee</a>and their
                     <br />work following this is a transcript of the interview.
@@ -33,68 +34,40 @@
             </div>
         </div>
         <!--begin::Aside-->
-        <!--begin::Body-->
         <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
-            <!--begin::Wrapper-->
             <div class="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
-                <!--begin::Content-->
                 <div class="d-flex flex-center flex-column align-items-stretch h-lg-100 w-md-400px">
-                    <!--begin::Wrapper-->
                     <div class="d-flex flex-center flex-column-fluid pb-15 pb-lg-20">
-                        <!--begin::Form-->
-                        <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" method="POST" action="{{ route('login') }}">
+                        <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form">
+                        <!-- <form class="form w-100" action="{{ route('login_process') }}" method="post"> -->
                             @csrf
-                            <!--begin::Heading-->
                             <div class="text-center mb-11">
-                                <!--begin::Title-->
                                 <h1 class="text-dark fw-bolder mb-3">PMKS</h1>
-                                <!--end::Title-->
-                                <!--begin::Subtitle-->
-                                <div class="text-gray-500 fw-semibold fs-6">Penyandang Masalah Kesejahteraan Sosial dan Tipiring</div>
-                                <!--end::Subtitle=-->
+                                <div class="text-gray-500 fw-semibold fs-6">Penyandang Masalah Kesejahteraan Sosial dan
+                                    Tipiring</div>
                             </div>
-                            <!--begin::Heading-->
-                            <!--begin::Input group=-->
                             <div class="fv-row mb-8">
-                                <!--begin::Email-->
                                 <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" />
-                                <!--end::Email-->
                             </div>
-                            <!--end::Input group=-->
                             <div class="fv-row mb-5">
-                                <!--begin::Password-->
                                 <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" />
-                                <!--end::Password-->
                             </div>
-                            <!--end::Input group=-->
-                            <!--begin::Wrapper-->
                             <!-- <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
                                 <div></div>
                                 <a href="../../demo39/dist/authentication/layouts/overlay/reset-password.html" class="link-primary">Forgot Password ?</a>
                             </div> -->
-                            <!--end::Wrapper-->
-                            <!--begin::Submit button-->
                             <div class="d-grid mb-10 pt-2">
-                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-                                    <!--begin::Indicator label-->
+                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary" onclick="submitForm()">
                                     <span class="indicator-label">Masuk</span>
-                                    <!--end::Indicator label-->
-                                    <!--begin::Indicator progress-->
                                     <span class="indicator-progress">Please wait...
                                         <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                                    <!--end::Indicator progress-->
                                 </button>
                             </div>
-                            <!--end::Submit button-->
-                            <!--begin::Sign up-->
                             <!-- <div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
                                 <a href="../../demo39/dist/authentication/layouts/overlay/sign-up.html" class="link-primary">Sign up</a>
                             </div> -->
-                            <!--end::Sign up-->
                         </form>
-                        <!--end::Form-->
                     </div>
-                    <!--end::Wrapper-->
                 </div>
             </div>
         </div>
@@ -105,4 +78,30 @@
 
 @push('scripts')
 <script src="dist/assets/js/custom/authentication/sign-in/general.js"></script>
+
+<script>
+    function submitForm() {
+        var formData = $('#kt_sign_in_form').serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: "{{ route('login_process') }}",
+            data: formData,
+            dataType: 'json', // ? jika server mengembalikan response dalam format JSON
+            success: function(response) {
+                // ? handle success response
+                console.log(response);
+                // ? redirect or perform other actions as needed
+                if (response.status == true) {
+                    window.location.href = response.redirect;
+                }
+            },
+            error: function(xhr, status, error) {
+                // ? handle error response
+                var errorMessage = xhr.responseJSON.message || 'Oops! Something went wrong.';
+                console.error(errorMessage);
+            },
+        });
+    }
+</script>
 @endpush
